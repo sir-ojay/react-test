8124b69ace44e4d1ec192c13c0e395682b92daec
ibrahim sannu <prince.ibrahim76@gmail.com>
2022-11-01 11:11:35 +0100

    Updates

---
+++ precache-manifest.c320adc6f1a7c436a06fe1f686d990bf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fcb1d3417ab5a40747854678d2c515d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30534794783dbb12b178",
+    "url": "/static/css/4.202fc531.chunk.css"
+  },
+  {
+    "revision": "f5f1119eb0779fc8370a",
+    "url": "/static/css/main.790a8141.chunk.css"
+  },
+  {
+    "revision": "30534794783dbb12b178",
+    "url": "/static/js/4.f8d8c535.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/4.f8d8c535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b8fa1a0a4138c2300a8",
+    "url": "/static/js/5.58e32697.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/static/js/5.58e32697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47e8025d755b11c8d633",
+    "url": "/static/js/6.604c3ad8.chunk.js"
+  },
+  {
+    "revision": "f5f1119eb0779fc8370a",
+    "url": "/static/js/main.01b411f9.chunk.js"
+  },
+  {
+    "revision": "1da861e9509e7e547f67",
+    "url": "/static/js/runtime-main.31b64ad4.js"
+  },
+  {
+    "revision": "4f0753a5e6af9080081f",
+    "url": "/static/js/stencil-h8k-logo_2-entry-js.2d0b81c7.chunk.js"
+  },
+  {
+    "revision": "f3b9275bd67f0179467d",
+    "url": "/static/js/stencil-polyfills-css-shim.5577844f.chunk.js"
+  }
+]);
