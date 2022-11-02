e3342a8992aaf47ee1e5510ef69c11fab1839f63
ibrahim sannu <prince.ibrahim76@gmail.com>
2022-11-02 12:39:52 +0100

    Updates

---
+++ precache-manifest.50d3a346fa1b2109539e9a5611e72b7f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1ea4fd5292b92a4a693458c3cca4a1",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "37af868d69cd57082697",
+    "url": "/react-test/static/css/4.202fc531.chunk.css"
+  },
+  {
+    "revision": "af14783deab7155485d9",
+    "url": "/react-test/static/css/main.790a8141.chunk.css"
+  },
+  {
+    "revision": "37af868d69cd57082697",
+    "url": "/react-test/static/js/4.a59d1a39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/4.a59d1a39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb630fe9eaca0e7a46f",
+    "url": "/react-test/static/js/5.69ee208b.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/react-test/static/js/5.69ee208b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4570476be756def57e94",
+    "url": "/react-test/static/js/6.4b3a28b0.chunk.js"
+  },
+  {
+    "revision": "af14783deab7155485d9",
+    "url": "/react-test/static/js/main.863c6e5e.chunk.js"
+  },
+  {
+    "revision": "3a1e95c2e1e0e270f7a6",
+    "url": "/react-test/static/js/runtime-main.4b356931.js"
+  },
+  {
+    "revision": "99123b8fd285e5014e44",
+    "url": "/react-test/static/js/stencil-h8k-logo_2-entry-js.053f74ec.chunk.js"
+  },
+  {
+    "revision": "cf733f61e82f5d61fa9d",
+    "url": "/react-test/static/js/stencil-polyfills-css-shim.5f396e8c.chunk.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5743208a3216c0316fb8eadbdc985dd3.js"
+  "/react-test/precache-manifest.50d3a346fa1b2109539e9a5611e72b7f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
